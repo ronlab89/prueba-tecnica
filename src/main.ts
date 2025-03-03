e435bb74f5c2f662fa455146f79b9255bd4a0075
Ronald Labrador <ronlabdev@gmail.com>
2025-03-03 14:38:28 -0500

    NEW: Initial project structure

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
